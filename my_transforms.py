56d6a117de7f1b74a719f3ab655d8a3cbef545b3
Valerio Ceccarelli <42637334+ValerioCeccarelli@users.noreply.github.com>
2023-07-15 23:15:09 +0200

    Update my_transforms.py

--- my_transforms.py
+++ my_transforms.py
@@ -42,12 +42,13 @@ class MyRandomHorizontalFlip():
 
 
 class MyRandomRotation():
-    def __init__(self, degrees: float = 90):
+    def __init__(self, degrees: float = 90, p: float = 0.5):
         self.degrees = degrees
+        self.p = p
 
     def __call__(self, imgs: tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]):
         img_10m, img_20m, img_60m, label = imgs
-        if np.random.rand() < 0.5:
+        if np.random.rand() < self.p:
             img_10m = F.rotate(img_10m, self.degrees)
             img_20m = F.rotate(img_20m, self.degrees)
             img_60m = F.rotate(img_60m, self.degrees)
